23d7b6205e803e4a7157dc464c64d8a8fdb779c2
K4rsion <k.gurin@g.nsu.ru>
2024-11-25 12:25:44 +0700

    init;

---
+++ src/pages/Main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Main = () => {
+  return (
+    <div>
+      <h1>Clean React App</h1>
+    </div>
+  );
+};
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import { default as viteReact } from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
+export default defineConfig({
+  plugins: [viteReact(), svgr()],
+  server: {
+    strictPort: true,
+  },
+  build: {
+    outDir: `build`,
+  },
+});
